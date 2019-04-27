980f90d45cf2a48014aa33ffc445d3c695a3ecc5
EssamKhames <48357878+EssamKhames@users.noreply.github.com>
2019-04-27 21:17:35 +0200

    Delete main2.java

--- main2.java
+++
@@ -1,9 +0,0 @@
-package eg.edu.alexu.csd.datastructure.stack.cs;
-
-public class main2 {
-	public static void main(String[]args) {
-//	Impor2 sasa = new Impor2();
-//	System.out.println( sasa.evaluate("666 2 / 3 - 4 2 * +") );
-//		System.out.print(sasa.infixToPostfix("a / b - c + d * e - a * c"));
-	}
-}
